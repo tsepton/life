9682f7cc03b67e7ab2b2cbf3a895b8cc6f3c7245
Thibaut Septon <tsepton@gmail.com>
2024-10-24 13:54:33 +0200

    Draw on board

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	type Cell = {
 		x: number;
 		y: number;
@@ -8,21 +10,39 @@
 
 	let board: HTMLCanvasElement;
 
-	let states: State[] = [];
+	let initializationState: State = [
+		{ x: 10, y: 9 },
+		{ x: 10, y: 10 },
+		{ x: 10, y: 11 }
+	];
 
-	function addCell(cell: Cell) {
-		states = [[...states[0], cell], ...states.slice(1)];
-	}
+	let currentState: State | undefined;
 
-	function removeCell(x: number, y: number) {
-		states = [states[0].filter((cell) => cell.x !== x && cell.y !== y), ...states.slice(1)];
-	}
+	let index = 0;
+
+	function addCell(cell: Cell) {}
+
+	function removeCell(x: number, y: number) {}
 
 	function computeNextState(state: State): State {
+		const computeOccurences = (cells: Cell[]): Map<string, number> => {
+			const occurences = new Map();
+			cells.forEach((cell) => {
+				const key = `${cell.x},${cell.y}`;
+				if (occurences.has(key)) occurences.set(key, occurences.get(key) + 1);
+				else occurences.set(key, 1);
+			});
+			return occurences;
+		};
+
+		console.log('state', state);
+
 		const augmentedState = state.map((cell) => {
 			const neighborsOnAxis = (n: number) => [n - 1, n, n + 1];
 			const neighbors = state
-				.filter((c) => c.x in neighborsOnAxis(cell.x) && c.y in neighborsOnAxis(cell.y))
+				.filter(
+					(c) => neighborsOnAxis(cell.x).includes(c.x) && neighborsOnAxis(cell.y).includes(c.y)
+				)
 				.filter((c) => c.x !== cell.x || c.y !== cell.y);
 			const hasRightAmountOfNeighbors = neighbors.length === 2 || neighbors.length === 3;
 			const getAllSurroundingCells = (cell: Cell): Cell[] => {
@@ -34,6 +54,7 @@
 
 			return [hasRightAmountOfNeighbors, surroundingDeadCells, cell];
 		});
+		console.log("augmentedState", augmentedState);
 
 		// Any live cell with fewer than two live neighbours dies, as if by underpopulation.
 		// Any live cell with two or three live neighbours lives on to the next generation.
@@ -44,23 +65,36 @@
 
 		// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 		const deadCellsDuplicates = augmentedState.flatMap(([_, deadCells]) => deadCells as Cell[]);
-		const reproductedCells = deadCellsDuplicates.filter(
+		const reproducedCells = deadCellsDuplicates.filter(
 			(c) => computeOccurences(deadCellsDuplicates).get(`${c.x},${c.y}`) === 3
 		);
-		return [...liveCells, ...reproductedCells];
+		return [...liveCells, ...reproducedCells];
 	}
 
-	function drawBoard() {}
+	function drawBoard(state: State): void {
+		const ctx = board.getContext('2d');
+		if (!ctx) return;
 
-	function computeOccurences(cells: Cell[]): Map<string, number> {
-		const occurences = new Map();
-		cells.forEach((cell) => {
-			const key = `${cell.x},${cell.y}`;
-			if (occurences.has(key)) occurences.set(key, occurences.get(key) + 1);
-			else occurences.set(key, 1);
+		ctx.reset();
+		state.forEach((cell) => {
+			ctx.fillStyle = 'black';
+			ctx.fillRect(cell.x * 25, cell.y * 25, 22.5, 22.5);
 		});
-		return occurences;
 	}
+
+	onMount(() => {
+		drawBoard(initializationState);
+
+		const interval = setInterval(() => {
+			currentState = computeNextState(currentState ?? initializationState);
+			index += 1;
+
+			drawBoard(currentState);
+		}, 1000);
+
+		return () => clearInterval(interval);
+	});
 </script>
 
-<canvas bind:this={board} width="800" height="800" style="border: 1px solid black;"></canvas>
+<canvas bind:this={board} width="700" height="700" style="border: 1px solid black;"></canvas>
+{index}
