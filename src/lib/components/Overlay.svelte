2857e9512199c426077eb0b6f6e4586290663a68
Thibaut Septon <tsepton@gmail.com>
2024-10-24 22:31:02 +0200

    Extract Overlay component

---
+++ src/lib/components/Overlay.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import board from '$lib/board';
+
+	export let speed = 10;
+</script>
+
+<div class="overlay-ui">
+	<div class="flex-col overlay-background">
+		Speed of simulation: {speed}
+		<input type="range" min="1" max="20" bind:value={speed} />
+	</div>
+	<div class="flex-col overlay-background">
+		<span> Generation: {$board.generation} </span>
+		<span> Population: {$board.state.length} </span>
+	</div>
+</div>
+
+<style>
+	.overlay-ui {
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		display: flex;
+		justify-content: space-between;
+		padding: 1rem;
+	}
+
+	.flex-col {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.overlay-background {
+		padding: 1rem;
+		border-radius: 5px;
+		background-color: rgba(226, 226, 226, 0.5);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Board } from '$lib/board';
 	import board from '$lib/board';
+	import Overlay from '$lib/components/Overlay.svelte';
 	import { onMount } from 'svelte';
 
 	let isClicked = false;
@@ -86,43 +87,13 @@
 
 <main>
 	<canvas bind:this={canvas}></canvas>
-	<div class="overlay-ui">
-		<div class="flex-col overlay-background">
-			Speed of simulation: {speed}
-			<input type="range" min="1" max="20" bind:value={speed} />
-		</div>
-		<div class="flex-col overlay-background">
-			<span> Generation: {$board.generation} </span>
-			<span> Population: {$board.state.length} </span>
-		</div>
-	</div>
+	<Overlay bind:speed={speed} />
 </main>
 
 <style>
 	main {
 		overflow: hidden;
 		height: 100vh;
-	}
-
-	.overlay-ui {
-		position: absolute;
-		top: 0;
-		left: 0;
-		right: 0;
-		display: flex;
-		justify-content: space-between;
-		padding: 1rem;
-	}
-
-	.flex-col {
-		display: flex;
-		flex-direction: column;
-	}
-
-	.overlay-background {
-		padding: 1rem;
-		border-radius: 5px;
-		background-color: rgba(226, 226, 226, 0.5);
 	}
 
 	canvas {
